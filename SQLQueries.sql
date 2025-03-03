8ff1f9d5f626c9d4b5789167d7792ad5dfb2c187
Abhishek Kushwaha <abhishek.kushwaha@quokkalabs.com>
2025-03-03 10:53:32 +0530

    Task 4 and 5 : Joins and Stored Procedures Completed

--- SQLQueries.sql
+++ SQLQueries.sql
@@ -1,5 +1,5 @@
 -- CREATING A TABLE
-
+/*
 CREATE TABLE Users (
     user_id INT PRIMARY KEY IDENTITY(1, 1),
     name VARCHAR(100) NOT NULL,
@@ -7,10 +7,10 @@ CREATE TABLE Users (
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     created_at DATETIME DEFAULT GETDATE()
-);
+); */
 
 -- INSERTING WITH DEFINING COLUMN NAMES AND INCREMENTING ID AUTOMATICALLY
-
+/*
 INSERT INTO Users (name, username, email, password, created_at) VALUES
 ('Alice Johnson', 'alicej', 'alice@example.com', '$2b$10$C6UUrT...', '2024-02-01 10:15:00'),
 ('Bob Smith', 'bobsmith', 'bob@example.com', '$2b$10$9qN1rU...', '2024-02-02 12:30:00'),
@@ -21,7 +21,7 @@ INSERT INTO Users (name, username, email, password, created_at) VALUES
 ('Grace Wilson', 'gracew', 'grace@example.com', '$2b$10$StUvWx...', '2024-02-07 11:55:00'),
 ('Henry Thomas', 'henryt', 'henry@example.com', '$2b$10$YzA1B2...', '2024-02-08 13:25:00'),
 ('Isabella Moore', 'isabellam', 'isabella@example.com', '$2b$10$C3D4E5...', '2024-02-09 16:30:00'),
-('Jack Anderson', 'jacka', 'jack@example.com', '$2b$10$F6G7H8...', '2024-02-10 19:10:00');
+('Jack Anderson', 'jacka', 'jack@example.com', '$2b$10$F6G7H8...', '2024-02-10 19:10:00'); */
 
 
 -- DISPLAYING ALL DETAILS OF Users TABLE 
@@ -31,18 +31,18 @@ SELECT * FROM Users;
 
 SET IDENTITY_INSERT Users ON;  -- Allow manual insertion into IDENTITY column
 
-INSERT INTO Users
+/*INSERT INTO Users
 (user_id, name, username, email, password, created_at)
 VALUES
 (35, 'Katherine White', 'kwhite', 'katherine@example.com', '$2b$10$I9J0K1...', '2024-02-11 08:00:00'),
 (42, 'Liam Harris', 'liamh', 'liam@example.com', '$2b$10$L2M3N4...', '2024-02-12 10:45:00');
--- If Done without IDENTITY_INSERT ON then it will throw an error and 'Column list' should be used
+-- If Done without IDENTITY_INSERT ON then it will throw an error and 'Column list' should be used */
 
 SET IDENTITY_INSERT Users OFF; -- We need to turn it off to allow insertion without column list
 
 -- INSERTING DATA INTO TABLE WITHOUT SPECIFYING THE COLUMN LIST
 
-INSERT INTO Users
+/* INSERT INTO Users
 VALUES
 ('Mia Robinson', 'miar', 'mia@example.com', '$2b$10$O5P6Q7...', '2024-02-13 14:55:00'),
 ('Noah Lewis', 'noahl', 'noah@example.com', '$2b$10$R8S9T0...', '2024-02-14 18:20:00'),
@@ -58,7 +58,7 @@ VALUES
 ('Xander Cooper', 'xanderc', 'xander@example.com', '$2b$10$V8W9X0...', '2024-02-24 15:50:00'),
 ('Zoe Ramirez', 'zoer', 'zoe@example.com', '$2b$10$Y1Z2A3...', '2024-02-25 18:10:00'),
 ('David Miller', 'davidm2', 'david2@example.com', '$2b$10$XyZ456...', '2024-02-26 14:45:00'),
-('Emily Davis', 'emilyd2', 'emily2@example.com', '$2b$10$MnOpQ2...', '2024-02-27 11:30:00');
+('Emily Davis', 'emilyd2', 'emily2@example.com', '$2b$10$MnOpQ2...', '2024-02-27 11:30:00'); */
 
 SELECT * FROM Users;
 
@@ -163,7 +163,7 @@ WHERE name NOT LIKE '%N';  -- Will give all records in which names are not endin
 
 	SELECT * FROM Users WHERE name = 'Abhishek Kushwaha';
 
-	UPDATE Users SET user_id = 91 WHERE user_id = 3;  -- Primary Keys cannot be updated
+	-- UPDATE Users SET user_id = 91 WHERE user_id = 3;  -- Primary Keys cannot be updated
 
 	UPDATE Users SET name = 'Barack Obama', username = 'President of US' WHERE user_id = 3;
 
@@ -213,7 +213,7 @@ WHERE name NOT LIKE '%N';  -- Will give all records in which names are not endin
 
 				-> Aggregate functions ignore the NULL values except for the count function
 */
-
+/*
 -- Sales Table
 CREATE TABLE Sales (
     SaleID INT PRIMARY KEY,
@@ -237,7 +237,7 @@ INSERT INTO Sales (SaleID, Product, Category, Quantity, Price, SaleDate, Store, 
 (7, 'Sofa', 'Furniture', 2, 30000, '2024-06-30', 'Store A', 'North'),
 (8, 'Table', 'Furniture', 3, 12000, '2024-07-10', 'Store B', 'South'),
 (9, 'Chair', 'Furniture', 4, 5000, '2024-08-15', 'Store C', 'East'),
-(10, 'Laptop', 'Electronics', 3, 59000, '2024-09-05', 'Store A', 'North');
+(10, 'Laptop', 'Electronics', 3, 59000, '2024-09-05', 'Store A', 'North'); */
 
 SELECT * FROM Sales;
 
@@ -249,7 +249,7 @@ SELECT Category, MIN(Price) AS MinPrice
 FROM Sales
 GROUP BY Category;
 
-SELECT Category, MIN(Price) AS MinPrice FROM Sales; -- MIN(Price) is an aggregate function that requires GROUP BY to compute the minimum price for each category.
+--SELECT Category, MIN(Price) AS MinPrice FROM Sales; -- MIN(Price) is an aggregate function that requires GROUP BY to compute the minimum price for each category.
 													-- Without GROUP BY, SQL does not know how to group data before applying MIN()
 
 SELECT COUNT(*) AS product_count FROM Sales WHERE pRODUCT = 'Laptop';
@@ -300,7 +300,7 @@ SELECT * FROM Sales;
 -- SQL JOINS
 
 -- JOINS -> used to combine rows from two or more tables, based on related column between them
-
+/*
 CREATE TABLE Customers (
     customer_id INT PRIMARY KEY,
     name VARCHAR(100),
@@ -332,7 +332,7 @@ INSERT INTO Orders VALUES
 (103, 1, '2024-03-05', 150.75),
 (104, 3, '2024-04-20', 300.00),
 (105, NULL, '2024-05-01', 500.00);
-
+*/
 
 SELECT * FROM Customers;
 
@@ -401,3 +401,355 @@ SELECT * FROM Orders;
 	SELECT name FROM Customers UNION SELECT order_id FROM Orders;  -- This is Invalid as name and order_id has different types
 
 	SELECT Customers.customer_id AS union_ids FROM Customers UNION SELECT Orders.order_id AS union_ids FROM Orders;
+
+
+	-- HAVING Clause
+			-- It is used as WHERE keyword cannot be used with aggregate functions
+
+			SELECT Product, COUNT(Quantity) AS totalCount, SUM(Quantity * Price) AS totalCost
+ 			FROM Sales
+			GROUP BY Product
+			HAVING SUM(Quantity * Price) > 20000;
+
+			SELECT SaleID, Product, Quantity,
+			CASE
+				WHEN Quantity < 2 THEN 'The quantity is less than 2'
+				WHEN Quantity = 3 THEN 'The quantity is 3'
+				ELSE 'The quantity is over 3'
+			END AS QuantityText
+			FROM Sales;
+
+
+
+
+-- Stored Procedures
+		-- It is a prepared SQL Code than you can save, so that the code can be reused again and again.
+		-- So If there is a SQL Query that you need to write again and again , save it as Stored Procedure the just call it to execute it
+
+		/*
+				Syntax
+
+				CREATE PROCEDURE procedureName
+				AS
+				sql_statement
+				GO;
+
+				EXEC procedureName;
+		*/
+
+		-- Creating a Procedure
+		CREATE PROCEDURE SelectAllSales
+		AS
+		SELECT * FROM Sales
+		GO;
+
+		-- Calling the Procedure
+		EXEC SelectAllSales;
+
+		CREATE PROCEDURE FilterSales @Product VARCHAR(20), @Price INT
+		AS
+		SELECT * FROM Sales WHERE Product = @Product AND Price = @Price;
+
+		EXEC FilterSales @Product = 'Laptop', @Price = 60000;
+
+
+		-- Creating Procedure to find User By ID
+
+		CREATE PROCEDURE GetUserById @userID INT
+		AS
+		BEGIN
+		SELECT * FROM Users WHERE user_id = @userID;
+		END
+
+		EXEC GetUserById @userID = 45;
+
+		-- Creating Procedure to find if User Exists in Table
+		CREATE PROCEDURE UserExists @userID INT
+		AS
+		BEGIN
+			IF NOT EXISTS (SELECT * FROM Users WHERE user_id = @userID)
+			BEGIN
+				PRINT 'User Does not exists!!!!'
+			END
+			ELSE
+			BEGIN
+				SELECT * FROM Users WHERE user_id = @userid;
+			END
+		END;
+
+		EXEC UserExists @userID = 100;
+
+
+
+-- STORE PROCEDURES EXAMPLES
+
+	CREATE TABLE Employees(
+		emp_id INT PRIMARY KEY NOT NULL,
+		emp_name VARCHAR(40) NOT NULL,
+		age INT
+	);
+
+	INSERT INTO Employees
+	(emp_id, emp_name, age)
+	VALUES
+	(1, 'Abhishek Kushwaha', 23);
+
+
+
+
+	-- ADD USER
+
+	CREATE PROCEDURE AddEmployee
+		@id INT,
+		@name VARCHAR(40),
+		@age INT
+	AS
+	BEGIN
+		INSERT INTO Employees
+		(emp_id, emp_name, age)
+		VALUES
+		(@id, @name, @age);
+	END;
+
+	EXEC AddEmployee 2, 'Andres', 54;
+	EXEC AddEmployee 3, 'Harry Potter', 15;
+	EXEC AddEmployee 4, 'Hermionie Granger', 16;
+	EXEC AddEmployee 5, 'Ron Weasely', 16;
+	EXEC AddEmployee 6, 'Ryan Reynolds', 23;
+
+
+
+
+	-- GET ALL EMPLOYEE DETAILS
+
+	CREATE PROCEDURE GetAllEmployeesDetails
+	AS
+	BEGIN
+		SELECT emp_id AS ID, emp_name AS EmployeeNAME, age AS EmployeeAge FROM Employees;
+	END;
+
+	DROP PROCEDURE GetAllEmployeesDetails;
+
+	EXEC GetAllEmployeesDetails;
+
+
+
+
+	-- Get Single Employee Details using ID
+
+	DROP PROCEDURE GetUserById;
+
+	CREATE PROCEDURE GetEmployeeByID @empID INT
+	AS
+	BEGIN
+		IF NOT EXISTS (SELECT * FROM Employees WHERE emp_id = @empID)
+		BEGIN
+			SELECT 'NO Employee assciated with this ID exists in database' AS Message;
+		END
+		ELSE
+		BEGIN
+			SELECT * FROM Employees WHERE emp_id = @empID;
+		END
+	END;
+
+
+	EXEC GetEmployeeByID 20;
+	EXEC GetEmployeeByID 1;
+
+
+
+	-- Update Employee Details
+
+	CREATE PROCEDURE UpdateEmployeeDetails
+		@id INT,
+		@name VARCHAR(40),
+		@age INT
+	AS
+	BEGIN
+		IF EXISTS (SELECT 1 FROM Employees WHERE emp_id = @id)
+		BEGIN
+			UPDATE Employees
+			SET emp_name = @name, age = @age
+			WHERE emp_id = @id;
+		END
+		ELSE
+		BEGIN
+			SELECT 'User does not exists with this id!!!' AS Message;
+		END
+	END;
+
+	EXEC UpdateEmployeeDetails 2, 'Professor', 45;
+
+	EXEC GetAllEmployeesDetails;
+
+
+
+	-- Delete Employee Details
+
+	CREATE PROCEDURE DeleteEmployeeDetails @empID INT
+	AS
+	BEGIN
+		IF EXISTS (SELECT 1 FROM Employees WHERE emp_id = @empID)
+		BEGIN
+			DELETE FROM Employees
+			WHERE emp_id = @empID;
+		END
+		ELSE
+		BEGIN
+			SELECT 'No Employee with this ID EXISTS!!!!' AS Message;
+		END
+	END;
+
+	EXEC DeleteEmployeeDetails 6;
+
+	EXEC GetAllEmployeesDetails;
+
+
+
+	-- Procedure to get count of all employees
+
+	CREATE PROCEDURE GetEmployeesCount
+	AS
+	BEGIN
+		SELECT COUNT(*) AS Total_Employees FROM Employees;
+	END;
+
+	DROP PROCEDURE GetEmployeesCount;
+
+	EXEC GetEmployeesCount;
+	EXEC GetAllEmployeesDetails;
+
+
+	-- Multiple Queries and Complex Queries for Stored Procedures
+
+	CREATE TABLE Departments (
+    dept_id INT PRIMARY KEY IDENTITY(1,1),
+    dept_name VARCHAR(50) NOT NULL
+	);
+
+	CREATE TABLE Salaries (
+    salary_id INT PRIMARY KEY IDENTITY(1,1),
+    emp_id INT,
+    amount DECIMAL(10,2),
+    effective_date DATE
+	);
+
+	CREATE TABLE Staff (
+    emp_id INT PRIMARY KEY IDENTITY(1,1),
+    emp_name VARCHAR(50) NOT NULL,
+    age INT CHECK (age >= 18),
+    dept_id INT,
+    salary_id INT
+	);
+
+
+	INSERT INTO Staff
+	(emp_name, age, dept_id, salary_id)
+	VALUES 
+	('John Doe', 30, 1, 1),
+	('Jane Smith', 25, 2, 2),
+	('Mike Johnson', 40, 1, 3),
+	('Emily Davis', 28, 3, 4);
+
+	INSERT INTO Departments (dept_name) VALUES 
+	('HR'),
+	('IT'),
+	('Finance');
+
+	INSERT INTO Salaries (emp_id, amount, effective_date) VALUES 
+	(1, 50000, '2024-01-01'),
+	(2, 60000, '2024-01-01'),
+	(3, 75000, '2024-01-01'),
+	(4, 55000, '2024-01-01');
+
+	SELECT * FROM Staff;
+	SELECT * FROM Departments;
+	SELECT * FROM Salaries;
+
+	SELECT * FROM Staff, Departments, Salaries;   -- > This Statement will by default give CROSS Join as Output Table
+
+	DROP Procedure GetEmployeesAndDepartments ;
+
+
+
+	-- Running Multiple Queries Together
+	CREATE PROCEDURE GetEmployeesAndDepartments @empID INT
+	AS
+	BEGIN
+		SELECT * FROM Staff WHERE emp_id = @empID;
+
+		SELECT * FROM Departments WHERE dept_id = (SELECT dept_id FROM Staff WHERE emp_id = @empID);
+
+		SELECT Staff.emp_name AS [Employee Name], Departments.dept_name as [Department Name], Salaries.amount AS Salary
+		FROM Staff
+		LEFT JOIN Departments ON Staff.dept_id = Departments.dept_id
+		LEFT JOIN Salaries ON Staff.emp_id = Salaries.emp_id
+		WHERE Staff.emp_id = @empID;
+	END;
+
+	EXEC GetEmployeesAndDepartments 1;
+
+
+	-- Using OUTPUT Parameters
+
+	DROP PROCEDURE GetEmployeeInfo;
+
+		CREATE PROCEDURE GetEmployeeInfo
+			@empID INT,
+			@empName VARCHAR(40) OUTPUT,
+			@empAge INT OUTPUT
+		AS
+		BEGIN
+			SELECT @empName = emp_name, @empAge = age FROM Staff WHERE emp_id = @empID;
+		END;
+
+		DECLARE @empName VARCHAR(40), @empAge INT;
+		EXEC GetEmployeeInfo 2, @empNAME OUTPUT, @empAge OUTPUT;
+		PRINT 'Employee Name : ' + @empName;
+		PRINT 'Employee Age : ' + CAST (@empAge AS VARCHAR);
+
+
+
+-- TRANSACTIONS USING STORED PROCEDURES
+
+		-- > Sequence of operations performed as a single unit of work ensuring ACID (Atomicity, Consistency, Isolation and Durability) properties.
+		-- > Used to maintain data Integrity
+
+		/*
+				Basic Structure of Transaction in a Stored Procedure
+
+					BEGIN TRANSACTION -> Starts a Transaction
+					COMMIT TRANSACTION -> Saves all Changes permanently
+					ROLLBACK TRANSACTION -> Reverts Changes in case of an error
+
+				Why Transactions?
+					1. Prevents inconsistent data (e.g., if the update fails mid-way).
+					2. Ensures reliability by rolling back changes if an error occurs.
+					3. Maintains data integrity in case of system failure.
+		*/
+
+		DROP PROCEDURE UpdateSalary;
+
+		-- Pricedure including Transaction
+		CREATE PROCEDURE UpdateSalary
+		@empID INT, @increment_percent DECIMAL(5, 2)
+		AS
+		BEGIN
+			BEGIN TRANSACTION;
+
+			BEGIN TRY
+				UPDATE Salaries
+				SET amount = amount + amount * @increment_percent / 100
+				WHERE emp_id = @empID;
+		
+				COMMIT;
+			END TRY
+			BEGIN CATCH
+				ROLLBACK;
+
+				PRINT 'ERROR OCCURRED : ' + ERROR_MESSAGE();
+			END CATCH
+		END;
+
+		EXEC UpdateSalary 1, 30;
+
